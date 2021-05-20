1189e2ebf0ea717195c77dffb615ae8c82ae5e54
ebmnm <ebmnm@aol.com>
2021-05-20 13:34:25 -0400

    fixes and animations

---
+++ index.js
@@ -0,0 +1,12 @@
+gsap.registerPlugin(ScrollTrigger)
+
+gsap.from(`.top`, {duration: 1, y: `-100%`})
+
+gsap.from(`nav`, {duration:1, opacity: 0})
+
+gsap.from(`.section-double`, { scrollTrigger: ".double-section",
+ x:-200, duration:2, opacity:.3, stagger: .3})
+
+ gsap.from(`.input-card`, {
+     y: 200, duration:1.5, opacity:.2, scrollTrigger:`.input-card`
+ })
